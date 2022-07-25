c39a1f87e5c5565aecbe5bd6a6a724d8aeaf5701
MuhammadRafi <108687316+Rafi12345pa@users.noreply.github.com>
2022-07-26 06:00:34 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,13 +1,11 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ┏──「 Donasi • Dana 」─⬣
-│ • Pulsa : 085850539404
-│ • Dana : 085850539404
+│ • Pulsa : 081252313064
+│ • Dana : 081252313064
 ╰────────⬣
 ┏──「 *NOTE* 」─⬣
-│ > Ingin donasi? wa.me/6285850539404
-│ _Hasil donasi akan digunakan buat sewa_
-│ _atau beli *RDP/VPS* agar bot bisa jalan_
-│ _24jam tanpa kendala_
+│ > Ingin donasi? wa.me/6283857620332
+│ _Hasil donasi akan digunakan buat beli kuota_
 ┗────────⬣
 `.trim(), wm, 'Menu', usedPrefix + 'menu', m) // Tambah sendiri kalo mau
 handler.help = ['donasi']
